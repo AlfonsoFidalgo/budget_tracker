9def42e20176fa06e16531fb11718cfac43058be
Alfonso Fidalgo <fidalgo.alfonso@gmail.com>
2024-11-09 19:40:42 +0100

    Add PUT route handler

--- src/routes/expenses.ts
+++ src/routes/expenses.ts
@@ -3,16 +3,18 @@ import { Expenses, Budgets } from "../data";
 
 const expensesRouter: Router = express.Router();
 
-interface CustomRequest extends Request {
+interface ExpenseRequest extends Request {
   newExpense?: {
     name: string;
     cost: number;
     budgetId: string;
   };
+  expenseId?: string;
+  expenseIndex?: number;
 }
 
 const validateExpense = (
-  req: CustomRequest,
+  req: ExpenseRequest,
   res: Response,
   next: NextFunction
 ): void => {
@@ -30,16 +32,67 @@ const validateExpense = (
   next();
 };
 
+expensesRouter.param(
+  "expenseId",
+  (req: ExpenseRequest, res: Response, next: NextFunction, id: string) => {
+    const expenseId = Number(id);
+    const expenseIndex = Expenses.findIndex(
+      (expense) => expense.id === expenseId
+    );
+    if (expenseIndex === -1) {
+      res.status(400).send("Invalid expenseId");
+      return;
+    }
+    req.expenseId = String(expenseId);
+    req.expenseIndex = expenseIndex;
+    next();
+  }
+);
+
 expensesRouter.get("/", (req: Request, res: Response) => {
   res.status(202).send(Expenses);
 });
 
-expensesRouter.post("/", validateExpense, (req: Request, res: Response) => {
-  const { name, cost, budgetId } = req.body;
-  const id = Expenses.length + 1;
-  const newExpense = { id, name, cost, budgetId };
-  Expenses.push(newExpense);
-  res.status(201).send(newExpense);
+expensesRouter.post(
+  "/",
+  validateExpense,
+  (req: ExpenseRequest, res: Response) => {
+    if (req.newExpense) {
+      const id = Expenses.length + 1;
+      const payload = {
+        ...req.newExpense,
+        id,
+        budgetId: Number(req.newExpense.budgetId),
+      };
+      Expenses.push(payload);
+      res.status(201).send(req.newExpense);
+    }
+  }
+);
+
+expensesRouter.delete("/:expenseId", (req: ExpenseRequest, res: Response) => {
+  if (req.expenseIndex) {
+    const deletedExpense = Expenses.splice(req.expenseIndex, 1);
+    res.send(deletedExpense);
+  }
 });
+
+expensesRouter.put(
+  "/:expenseId",
+  validateExpense,
+  (req: ExpenseRequest, res: Response) => {
+    if (req.expenseIndex) {
+      const updatedExpense = {
+        id: Expenses[req.expenseIndex].id,
+        name: req.body.name,
+        cost: req.body.cost,
+        budgetId: req.body.budgetId,
+      };
+      Expenses[req.expenseIndex] = updatedExpense;
+      res.send(updatedExpense);
+    }
+    res.send();
+  }
+);
 
 export default expensesRouter;
