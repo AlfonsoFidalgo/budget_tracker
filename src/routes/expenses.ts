d4c7c83ac52c14638dfbfac8faffb978502e9f39
Alfonso Fidalgo <fidalgo.alfonso@gmail.com>
2024-11-09 17:16:24 +0100

    Add middleware expense validation

--- src/routes/expenses.ts
+++ src/routes/expenses.ts
@@ -1,13 +1,40 @@
-import express, { Request, Response, Router } from "express";
-import { Expenses } from "../data";
+import express, { Request, Response, Router, NextFunction } from "express";
+import { Expenses, Budgets } from "../data";
 
 const expensesRouter: Router = express.Router();
+
+interface CustomRequest extends Request {
+  newExpense?: {
+    name: string;
+    cost: number;
+    budgetId: string;
+  };
+}
+
+const validateExpense = (
+  req: CustomRequest,
+  res: Response,
+  next: NextFunction
+): void => {
+  const { name, cost, budgetId } = req.body;
+  if (typeof cost !== "number") {
+    res.status(400).send("Bad input type");
+    return;
+  }
+  const budget = Budgets.find((b) => b.id === budgetId);
+  if (!budget) {
+    res.status(400).send("Inexisting budget");
+    return;
+  }
+  req.newExpense = { name, cost, budgetId };
+  next();
+};
 
 expensesRouter.get("/", (req: Request, res: Response) => {
   res.status(202).send(Expenses);
 });
 
-expensesRouter.post("/", (req: Request, res: Response) => {
+expensesRouter.post("/", validateExpense, (req: Request, res: Response) => {
   const { name, cost, budgetId } = req.body;
   const id = Expenses.length + 1;
   const newExpense = { id, name, cost, budgetId };
