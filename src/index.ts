4fd6ad5cbce52a944cbbd6c3959799207003c1e7
Alfonso Fidalgo <fidalgo.alfonso@gmail.com>
2024-11-09 15:40:02 +0100

    Add budgets file with dummy data

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,10 @@
 import express, { Express, Request, Response } from "express";
+import budgetsRouter from "./routes/budgets";
 
 const app: Express = express();
 const PORT: number = 8000;
 
-app.get("/", (req: Request, res: Response) => {
-  res.status(202).send({response: "Hello World!"});
-});
+app.use("/api/budgets", budgetsRouter);
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
--- src/routes/budgets.ts
+++ src/routes/budgets.ts
@@ -1,4 +1,10 @@
-import express, { Express, Request, Response, Router } from "express";
+import express, { Request, Response, Router } from "express";
+import { Budgets } from "../data";
 
 const budgetsRouter: Router = express.Router();
 
+budgetsRouter.get("/", (req: Request, res: Response) => {
+  res.status(202).send(Budgets);
+});
+
+export default budgetsRouter;
