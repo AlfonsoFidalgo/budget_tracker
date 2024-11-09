030985cbf926418c773170a0aaba5a107981f66c
Alfonso Fidalgo <fidalgo.alfonso@gmail.com>
2024-11-09 15:59:19 +0100

    Add GET budget route

--- src/routes/budgets.ts
+++ src/routes/budgets.ts
@@ -7,10 +7,17 @@ budgetsRouter.get("/", (req: Request, res: Response) => {
   res.status(202).send(Budgets);
 });
 
+budgetsRouter.get("/:budgetId", (req: Request, res: Response) => {
+  const budget = Budgets.filter(
+    (budget) => budget.id === Number(req.params.budgetId)
+  );
+  res.send(budget);
+});
+
 budgetsRouter.get("/total", (req: Request, res: Response) => {
-    const total = Budgets.reduce((a, b) => a + b.budget, 0)
-    res.send({total: total})
-})
+  const total = Budgets.reduce((a, b) => a + b.budget, 0);
+  res.send({ total });
+});
 
 budgetsRouter.post("/", (req: Request, res: Response) => {
   const { name, budget, description } = req.body;
